6f7c4aecd2c9e3d54f5d0c3bd25c0bb9e3f6dd40
ogicoco <ogicoco.english@gmail.com>
2026-09-16 11:51:06 +0900

    Lower lesson photo crop slightly

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const learningModeTitle=document.querySelector('.split-photo h3');
 if(learningModeTitle){learningModeTitle.innerHTML='「おぎココに来ると、<br>学習モードに。」'}
 
 const lessonSceneImage=document.querySelector('img[src="assets/images/students-studying.jpg"]');
-if(lessonSceneImage){lessonSceneImage.style.objectPosition='center 30%'}
+if(lessonSceneImage){lessonSceneImage.style.objectPosition='center 35%'}
 
 document.querySelectorAll('a[href*="page.line.me/971ktbrj"]').forEach((link,index)=>{
   link.addEventListener('click',()=>{
